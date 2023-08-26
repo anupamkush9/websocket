8566493f6836ad2cac48699ce41ed8a4d2617b2b
Anupamkush9 <anupam.kush9@gmail.com>
2023-08-27 01:06:44 +0530

    basic websocket implemented with websocket consumer

--- websocket_app/consumers.py
+++ websocket_app/consumers.py
@@ -6,6 +6,7 @@ from channels.exceptions import StopConsumer
 import time
 from websocket_app.models import Chat, Group
 from channels.db import database_sync_to_async
+from channels.generic.websocket import WebsocketConsumer
 
 class MySyncConsumer(SyncConsumer):
 
@@ -141,3 +142,32 @@ class MyAsyncConsumer(AsyncConsumer):
             self.room_name, self.channel_name
         )
         raise StopConsumer()
+
+
+class MyWebsocketConsumer(WebsocketConsumer):
+
+    def connect(self):
+        # Called on connection.
+        # To accept the connection call:
+        self.accept()
+        # # Or accept the connection and specify a chosen subprotocol.
+        # # A list of subprotocols specified by the connecting client
+        # # will be available in self.scope['subprotocols']
+        # self.accept("subprotocol")
+        # To reject the connection, call:
+        # self.close()
+
+    def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        self.send(text_data="Hello world!")
+        # Or, to send a binary frame:
+        # self.send(bytes_data="Hello world!")
+        # Want to force-close the connection? Call:
+        # self.close()
+        # Or add a custom WebSocket error code!
+        self.close(code=4123)
+
+    def disconnect(self, close_code):
+        print("Disconnecting.........")
+        # Called when the socket closes
--- websocket_app/routing.py
+++ websocket_app/routing.py
@@ -1,9 +1,11 @@
 # chat/routing.py
 from django.urls import re_path
 
-from websocket_app.consumers import MySyncConsumer, MyAsyncConsumer
+from websocket_app.consumers import MySyncConsumer, MyAsyncConsumer, MyWebsocketConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/sync_consumer/(?P<room_name>\w+)/$', MySyncConsumer.as_asgi()),
     re_path(r"ws/async_consumer/(?P<room_name>\w+)/$", MyAsyncConsumer.as_asgi()),
+    re_path(r"ws/websocket_consumer/$", MyWebsocketConsumer.as_asgi()),
+    
 ]
