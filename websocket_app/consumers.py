dc79aa9362b9f4e6fea04067ed6edd48834adcb5
Anupamkush9 <anupam.kush9@gmail.com>
2023-08-27 12:46:48 +0530

    chat application by WebsocketConsumer

--- websocket_app/consumers.py
+++ websocket_app/consumers.py
@@ -4,6 +4,8 @@ from channels.consumer import SyncConsumer
 from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
 import time
+from channels.generic.websocket import WebsocketConsumer
+
 
 class MySyncConsumer(SyncConsumer):
 
@@ -113,3 +115,53 @@ class MyAsyncConsumer(AsyncConsumer):
             self.room_name, self.channel_name
         )
         raise StopConsumer()
+
+
+class MyWebsocketConsumer(WebsocketConsumer):
+    # groups = ["broadcast"]
+
+    def connect(self):
+        # Called on connection.
+        # To accept the connection call:
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name, self.channel_name
+        )
+        self.accept()
+        # Or accept the connection and specify a chosen subprotocol.
+        # A list of subprotocols specified by the connecting client
+        # will be available in self.scope['subprotocols']
+        # self.accept("subprotocol")
+        # To reject the connection, call:
+        # self.close()
+
+    def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        client_data = json.loads(text_data)
+
+        
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name, {"type": "chat.message", "message": client_data}
+        )
+
+        # Or, to send a binary frame:
+        # self.send(bytes_data="Hello world!")
+        # Want to force-close the connection? Call:
+        # self.close()
+        # Or add a custom WebSocket error code!
+        # self.close(code=4123)
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps(message))
+
+
+    def disconnect(self, close_code):
+        print("Websocket disconnected...")
+        # Called when the socket closes
