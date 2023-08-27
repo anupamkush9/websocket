200966d4e140a5b8dda74017a23ed037bca34e84
Anupamkush9 <anupam.kush9@gmail.com>
2023-08-27 23:25:30 +0530

    counter_with_ui_with_JsonWebsocketConsumer_and_AsyncJsonWebsocketConsumer

--- websocket_app/consumers.py
+++ websocket_app/consumers.py
@@ -7,150 +7,214 @@ import time
 import asyncio
 from websocket_app.models import Chat, Group
 from channels.db import database_sync_to_async
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer, JsonWebsocketConsumer, AsyncJsonWebsocketConsumer
 
-class MySyncConsumer(SyncConsumer):
+# class MySyncConsumer(SyncConsumer):
 
-    def websocket_connect(self, event):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_name, self.channel_name
-        )
-        self.send({
-            "type": "websocket.accept",
-        })
-        print("sync websocket connection established")
+#     def websocket_connect(self, event):
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         # Join room group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_name, self.channel_name
+#         )
+#         self.send({
+#             "type": "websocket.accept",
+#         })
+#         print("sync websocket connection established")
 
-    def websocket_receive(self, event):
-        print("event:::", event)
-        print("eve type nt:::", type(event))
-        print("dir(event):::", dir(event))
-        text_data_json = json.loads(event.get("text"))
-        print("text_data_json:::", text_data_json)
-        print("text_data_json:::", type(text_data_json))
-        message = text_data_json["message"]
-        if self.scope['user'].is_authenticated :
-            group = Group.objects.get(name = self.room_name)
-            print("------->",group)
-            print("---type---->",type(group))
-            chat = Chat(content=text_data_json["message"],
-                        group=group)
-            chat.save()
+#     def websocket_receive(self, event):
+#         print("event:::", event)
+#         print("eve type nt:::", type(event))
+#         print("dir(event):::", dir(event))
+#         text_data_json = json.loads(event.get("text"))
+#         print("text_data_json:::", text_data_json)
+#         print("text_data_json:::", type(text_data_json))
+#         message = text_data_json["message"]
+#         if self.scope['user'].is_authenticated :
+#             group = Group.objects.get(name = self.room_name)
+#             print("------->",group)
+#             print("---type---->",type(group))
+#             chat = Chat(content=text_data_json["message"],
+#                         group=group)
+#             chat.save()
 
-            # Send message to room group
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_name, {"type": "chat.message", "message": text_data_json}
-            )
-        else:
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_name, {"type": "chat.message", "message": {"user": "Annonymous user", "message": "Login Required"}}
-            )
-    # Receive message from room group
-    def chat_message(self, event):
-        print("chat_message event:::", event)
-        print("chat_message eve type nt:::", type(event))
-        print("chat_message dir(event):::", dir(event))
-        text_data_json = event.get("message")
-        print("text_data_json:::", text_data_json)
-        print("text_data_json:::", type(text_data_json))
-        message = text_data_json["message"]
-        print("emssage:::", message)
-        print("typeof message", type(message))
+#             # Send message to room group
+#             async_to_sync(self.channel_layer.group_send)(
+#                 self.room_name, {"type": "chat.message", "message": text_data_json}
+#             )
+#         else:
+#             async_to_sync(self.channel_layer.group_send)(
+#                 self.room_name, {"type": "chat.message", "message": {"user": "Annonymous user", "message": "Login Required"}}
+#             )
+#     # Receive message from room group
+#     def chat_message(self, event):
+#         print("chat_message event:::", event)
+#         print("chat_message eve type nt:::", type(event))
+#         print("chat_message dir(event):::", dir(event))
+#         text_data_json = event.get("message")
+#         print("text_data_json:::", text_data_json)
+#         print("text_data_json:::", type(text_data_json))
+#         message = text_data_json["message"]
+#         print("emssage:::", message)
+#         print("typeof message", type(message))
 
-        # Send message to WebSocket
-        # self.send(json.dumps({"message": message}))
-        self.send({
-                "type": "websocket.send",
-                "text": message,
-            })
+#         # Send message to WebSocket
+#         # self.send(json.dumps({"message": message}))
+#         self.send({
+#                 "type": "websocket.send",
+#                 "text": message,
+#             })
 
-    def websocket_disconnect(self, event):
-        print("sync websocket consumer disconnect")
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_name, self.channel_name
-        )
-        raise StopConsumer()
-
-
-class MyAsyncConsumer(AsyncConsumer):
-
-    async def websocket_connect(self, event):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_name, self.channel_name
-        )
-        await self.send({
-            "type": "websocket.accept",
-        })
-        print("Async websocket connection established")
-
-    async def websocket_receive(self, event):
-        print("event:::", event)
-        print("eve type nt:::", type(event))
-        print("dir(event):::", dir(event))
-        text_data_json = json.loads(event.get("text"))
-        print("text_data_json:::", text_data_json)
-        print("text_data_json:::", type(text_data_json))
-        message = text_data_json["message"]
-
-        print("--user-----_>", self.scope['user'])
-        print("--username-----_>", self.scope['user'].username)
-        print("--dir(user)-----_>", dir(self.scope['user']))
-        print("---.self.scope['user'].is_authenticated----", self.scope['user'].is_authenticated)
-        if self.scope['user'].is_authenticated :
-            group  = await database_sync_to_async(Group.objects.get)(name = self.room_name)
-            print("------->",group)
-            print("---type---->",type(group))
-            chat = Chat(content=text_data_json["message"],
-                        group=group)
-            await database_sync_to_async(chat.save)()
-            text_data_json["user"] = self.scope['user'].username
-            # Send message to room group
-            await self.channel_layer.group_send(
-                self.room_name, {"type": "chat.message", "message": text_data_json}
-            )
-        else:
-            await self.channel_layer.group_send(
-                self.room_name, {"type": "chat.message", "message": {"user": "Annonymous user", "message": "Login Required"}}
-            )
+#     def websocket_disconnect(self, event):
+#         print("sync websocket consumer disconnect")
+#         # Leave room group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_name, self.channel_name
+#         )
+#         raise StopConsumer()
 
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        print("chat_message event:::", event)
-        print("chat_message eve type nt:::", type(event))
-        print("chat_message dir(event):::", dir(event))
-        text_data_json = event.get("message")
-        print("text_data_json:::", text_data_json)
-        print("text_data_json:::", type(text_data_json))
-        message = text_data_json["message"]
-        print("emssage:::", message)
-        print("typeof message", type(message))
+# class MyAsyncConsumer(AsyncConsumer):
 
-        # Send message to WebSocket
-        await self.send({
-                "type": "websocket.send",
-                "text": json.dumps(text_data_json),
-            })
+#     async def websocket_connect(self, event):
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.room_name, self.channel_name
+#         )
+#         await self.send({
+#             "type": "websocket.accept",
+#         })
+#         print("Async websocket connection established")
 
-    async def websocket_disconnect(self, event):
-        print("sync websocket consumer disconnect")
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_name, self.channel_name
-        )
-        raise StopConsumer()
+#     async def websocket_receive(self, event):
+#         print("event:::", event)
+#         print("eve type nt:::", type(event))
+#         print("dir(event):::", dir(event))
+#         text_data_json = json.loads(event.get("text"))
+#         print("text_data_json:::", text_data_json)
+#         print("text_data_json:::", type(text_data_json))
+#         message = text_data_json["message"]
+
+#         print("--user-----_>", self.scope['user'])
+#         print("--username-----_>", self.scope['user'].username)
+#         print("--dir(user)-----_>", dir(self.scope['user']))
+#         print("---.self.scope['user'].is_authenticated----", self.scope['user'].is_authenticated)
+#         if self.scope['user'].is_authenticated :
+#             group  = await database_sync_to_async(Group.objects.get)(name = self.room_name)
+#             print("------->",group)
+#             print("---type---->",type(group))
+#             chat = Chat(content=text_data_json["message"],
+#                         group=group)
+#             await database_sync_to_async(chat.save)()
+#             text_data_json["user"] = self.scope['user'].username
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_name, {"type": "chat.message", "message": text_data_json}
+#             )
+#         else:
+#             await self.channel_layer.group_send(
+#                 self.room_name, {"type": "chat.message", "message": {"user": "Annonymous user", "message": "Login Required"}}
+#             )
 
 
-class MyWebsocketConsumer(WebsocketConsumer):
+#     # Receive message from room group
+#     async def chat_message(self, event):
+#         print("chat_message event:::", event)
+#         print("chat_message eve type nt:::", type(event))
+#         print("chat_message dir(event):::", dir(event))
+#         text_data_json = event.get("message")
+#         print("text_data_json:::", text_data_json)
+#         print("text_data_json:::", type(text_data_json))
+#         message = text_data_json["message"]
+#         print("emssage:::", message)
+#         print("typeof message", type(message))
+
+#         # Send message to WebSocket
+#         await self.send({
+#                 "type": "websocket.send",
+#                 "text": json.dumps(text_data_json),
+#             })
+
+#     async def websocket_disconnect(self, event):
+#         print("sync websocket consumer disconnect")
+#         # Leave room group
+#         await self.channel_layer.group_discard(
+#             self.room_name, self.channel_name
+#         )
+#         raise StopConsumer()
+
+
+# class MyWebsocketConsumer(WebsocketConsumer):
+
+#     def connect(self):
+#         # To accept the connection call:
+#         # Called on connection.
+#         print("connection established", self)
+#         self.accept()
+#         # # Or accept the connection and specify a chosen subprotocol.
+#         # # A list of subprotocols specified by the connecting client
+#         # # will be available in self.scope['subprotocols']
+#         # self.accept("subprotocol")
+#         # To reject the connection, call:
+#         # self.close()
+
+#     def receive(self, text_data=None, bytes_data=None):
+#         # Called with either text_data or bytes_data for each frame
+#         # You can call:
+#         print("text_data:::", text_data)
+#         for i in range(20):
+#             time.sleep(1)
+#             self.send(text_data=json.dumps({"message":str(i)}))
+#         # Or, to send a binary frame:
+#         # self.send(bytes_data="Hello world!")
+#         # Want to force-close the connection? Call:
+#         # self.close()
+#         # Or add a custom WebSocket error code!
+#         # self.close(code=4123)
+
+#     # def disconnect(self, close_code):
+#     #     print("Disconnecting.........")
+#         # Called when the socket closes
+
+# class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
+
+#     async def connect(self):
+#         # To accept the connection call:
+#         # Called on connection.
+#         print("connection established", self)
+#         await self.accept()
+#         # # Or accept the connection and specify a chosen subprotocol.
+#         # # A list of subprotocols specified by the connecting client
+#         # # will be available in self.scope['subprotocols']
+#         # self.accept("subprotocol")
+#         # To reject the connection, call:
+#         # self.close()
+
+#     async def receive(self, text_data=None, bytes_data=None):
+#         # Called with either text_data or bytes_data for each frame
+#         # You can call:
+#         print("text_data:::", text_data)
+#         for i in range(20):
+#             await self.send(text_data=json.dumps({"message":str(i)}))
+#             await asyncio.sleep(1)
+#         # Or, to send a binary frame:
+#         # self.send(bytes_data="Hello world!")
+#         # Want to force-close the connection? Call:
+#         # self.close()
+#         # Or add a custom WebSocket error code!
+#         # self.close(code=4123)
+
+#     async def disconnect(self, close_code):
+#         print("Disconnecting.........")
+#         # Called when the socket closes
+
+
+class MyJsonWebsocketConsumer(JsonWebsocketConsumer):
 
     def connect(self):
         # To accept the connection call:
         # Called on connection.
-        print("connection established", self)
         self.accept()
         # # Or accept the connection and specify a chosen subprotocol.
         # # A list of subprotocols specified by the connecting client
@@ -159,13 +223,13 @@ class MyWebsocketConsumer(WebsocketConsumer):
         # To reject the connection, call:
         # self.close()
 
-    def receive(self, text_data=None, bytes_data=None):
+    def receive_json(self, content=None, **kwargs):
         # Called with either text_data or bytes_data for each frame
         # You can call:
-        print("text_data:::", text_data)
+        print("text_data:::", content)
         for i in range(20):
             time.sleep(1)
-            self.send(text_data=json.dumps({"message":str(i)}))
+            self.send_json({"message":i})
         # Or, to send a binary frame:
         # self.send(bytes_data="Hello world!")
         # Want to force-close the connection? Call:
@@ -175,9 +239,9 @@ class MyWebsocketConsumer(WebsocketConsumer):
 
     # def disconnect(self, close_code):
     #     print("Disconnecting.........")
-        # Called when the socket closes
+    #     # Called when the socket closes
 
-class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
+class MyAsyncJsonWebsocketConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
         # To accept the connection call:
@@ -191,12 +255,12 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
         # To reject the connection, call:
         # self.close()
 
-    async def receive(self, text_data=None, bytes_data=None):
+    async def receive_json(self, content):
         # Called with either text_data or bytes_data for each frame
         # You can call:
-        print("text_data:::", text_data)
+        print("text_data:::", content)
         for i in range(20):
-            await self.send(text_data=json.dumps({"message":str(i)}))
+            await self.send_json({"message":str(i)})
             await asyncio.sleep(1)
         # Or, to send a binary frame:
         # self.send(bytes_data="Hello world!")
--- websocket_app/routing.py
+++ websocket_app/routing.py
@@ -1,12 +1,14 @@
 # chat/routing.py
 from django.urls import re_path
 
-from websocket_app.consumers import MySyncConsumer, MyAsyncConsumer, MyWebsocketConsumer, MyAsyncWebsocketConsumer
+from websocket_app.consumers import MyJsonWebsocketConsumer, MyAsyncJsonWebsocketConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/sync_consumer/(?P<room_name>\w+)/$', MySyncConsumer.as_asgi()),
-    re_path(r"ws/async_consumer/(?P<room_name>\w+)/$", MyAsyncConsumer.as_asgi()),
-    re_path(r"ws/websocket_consumer/$", MyWebsocketConsumer.as_asgi()),
-    re_path(r"ws/asyncwebsocket_consumer/$", MyAsyncWebsocketConsumer.as_asgi()),
+    # re_path(r'ws/sync_consumer/(?P<room_name>\w+)/$', MySyncConsumer.as_asgi()),
+    # re_path(r"ws/async_consumer/(?P<room_name>\w+)/$", MyAsyncConsumer.as_asgi()),
+    # re_path(r"ws/websocket_consumer/$", MyWebsocketConsumer.as_asgi()),
+    # re_path(r"ws/asyncwebsocket_consumer/$", MyAsyncWebsocketConsumer.as_asgi()),
+    re_path(r"ws/My_JsonWebsocketConsumer/$", MyJsonWebsocketConsumer.as_asgi()),
+    re_path(r"ws/My_AsyncJsonWebsocketConsumer/$", MyAsyncJsonWebsocketConsumer.as_asgi()),
     
 ]
