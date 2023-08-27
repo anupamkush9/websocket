95160e24744d47fa0c84ea2428312ea09b467a0b
Anupamkush9 <anupam.kush9@gmail.com>
2023-08-27 23:44:51 +0530

    chat application with database by JsonWebsocketConsumer

--- websocket_app/consumers.py
+++ websocket_app/consumers.py
@@ -5,7 +5,7 @@ from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
 import time
 from .models import Chat, Group
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
 
 
 class MySyncConsumer(SyncConsumer):
@@ -167,6 +167,63 @@ class MyWebsocketConsumer(WebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps(message))
+
+
+    def disconnect(self, close_code):
+        print("Websocket disconnected...")
+        # Called when the socket closes
+
+class MyJsonWebsocketConsumer(JsonWebsocketConsumer):
+    # groups = ["broadcast"]
+
+    def connect(self):
+        # Called on connection.
+        # To accept the connection call:
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name, self.channel_name
+        )
+        self.accept()
+        # Or accept the connection and specify a chosen subprotocol.
+        # A list of subprotocols specified by the connecting client
+        # will be available in self.scope['subprotocols']
+        # self.accept("subprotocol")
+        # To reject the connection, call:
+        # self.close()
+
+    def receive_json(self,content = None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        # client_data = json.loads(text_data)
+        print("-->", content)
+        print("-->", type(content))
+
+        group = Group.objects.get(name = self.room_name)
+        print("------->",group)
+        print("---type---->",type(group))
+        chat = Chat(content=content.get("message"),
+                    group=group)
+        chat.save()
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name, {"type": "chat.message", "message": content}
+        )
+
+        # Or, to send a binary frame:
+        # self.send(bytes_data="Hello world!")
+        # Want to force-close the connection? Call:
+        # self.close()
+        # Or add a custom WebSocket error code!
+        # self.close(code=4123)
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send_json(message)
 
 
     def disconnect(self, close_code):
--- websocket_app/routing.py
+++ websocket_app/routing.py
@@ -1,10 +1,11 @@
 # chat/routing.py
 from django.urls import re_path
 
-from websocket_app.consumers import MySyncConsumer, MyAsyncConsumer, MyWebsocketConsumer
+from websocket_app.consumers import MySyncConsumer, MyAsyncConsumer, MyWebsocketConsumer, MyJsonWebsocketConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/sync_consumer/(?P<room_name>\w+)/$', MySyncConsumer.as_asgi()),
     re_path(r"ws/async_consumer/(?P<room_name>\w+)/$", MyAsyncConsumer.as_asgi()),
     re_path(r"ws/MyWebsocketConsumer/(?P<room_name>\w+)/$", MyWebsocketConsumer.as_asgi()),
+    re_path(r"ws/MyJsonWebsocketConsumer/(?P<room_name>\w+)/$", MyJsonWebsocketConsumer.as_asgi()),
 ]
